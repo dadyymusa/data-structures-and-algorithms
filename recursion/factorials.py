1671a5c1b535de0161d155325aee6494da216270
Musa Sudad <musasudad6@gmail.com>
2026-01-17 02:54:40 +0600

    Factorials

---
+++ recursion/factorials.py
@@ -0,0 +1,13 @@
+def factorials(n):
+    # remove this line if you hate your pc
+    if n < 0 or n > 25:  
+        return '-1'
+    elif n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    return n * factorials(n - 1)
+
+num = 9
+
+print(factorials(num))
