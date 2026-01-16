86956baae39a7eab2ec587eea8a9fd1f2bc292f5
Musa Sudad <musasudad6@gmail.com>
2026-01-17 02:30:20 +0600

    Recursion - Power of Four

---
+++ recursion/leetcode-342.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/power-of-four/description/?envType=problem-list-v2&envId=recursion
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        
+        if n == 1:
+            return True
+        elif n % 4 != 0:
+            return False
+
+        return self.isPowerOfFour(n / 4)
